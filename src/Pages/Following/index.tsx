7412fa8f465a285aa207057b40ea3418a701df5a
indominuShot <luang193@gmail.com>
2020-12-28 15:04:22 -0300

    Página Following atualizada com os itens

--- src/Pages/Following/index.tsx
+++ src/Pages/Following/index.tsx
@@ -1,15 +1,94 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { FlatList } from 'react-native';
+import Header from '../../Components/Header';
+import Heading from '../../Components/Heading';
+import Title from '../../Components/Title';
+import CategoriesList from '../../Components/CategoriesList';
 
-import { Container } from './styles';
+import { Container, Wrapper, Main } from './styles';
+import StreamList from '../../Components/StreamList';
+import ChannelList from '../../Components/ChannelList';
+
+interface _Item {
+	key: string;
+	render: () => JSX.Element;
+	isTitle?: boolean;
+}
 
 const Following: React.FC = () => {
-  return (
-    <Container>
-      <Text>Following</Text>
-    </Container>
-  );
+	const { data, indexes } = React.useMemo(() => {
+		const items: _Item[] = [
+			{
+				key: 'PAGE_HEADING',
+				render: () => <Heading>Following</Heading>,
+			},
+			{
+				key: 'FOLLOWED_cATEGORIES',
+				render: () => <Title>Followed categories</Title>,
+				isTitle: true,
+			},
+			{
+				key: 'c1',
+				render: () => <CategoriesList />,
+			},
+			{
+				key: 'LIVE_CHANNELS',
+				render: () => <Title>Live channels</Title>,
+				isTitle: true,
+			},
+			{
+				key: 'c2',
+				render: () => <StreamList />,
+			},
+			{
+				key: 'CONTINUE_WATCHING',
+				render: () => <Title>Continue watching</Title>,
+				isTitle: true,
+			},
+			{
+				key: 'c3',
+				render: () => <StreamList />,
+			},
+			{
+				key: 'OFFLINE_CHANNELS',
+				render: () => <Title>Offline channels</Title>,
+				isTitle: true,
+			},
+			{
+				key: 'c4',
+				render: () => <ChannelList />,
+			},
+		];
+
+		const indexes: number[] = [];
+
+		items.forEach((item, index) => item.isTitle && indexes.push(index));
+
+		return {
+			data: items,
+			indexes,
+		};
+	}, []);
+
+	return (
+		<Wrapper>
+			<Header />
+
+			<Container>
+				<Main>
+					<FlatList<_Item>
+						data={data}
+						renderItem={({ item }) => item.render()}
+						keyExtractor={(item) => item.key}
+						stickyHeaderIndices={indexes}
+						onRefresh={() => {}}
+						refreshing={false}
+					/>
+				</Main>
+			</Container>
+		</Wrapper>
+	);
 };
 
 export default Following;
--- src/Pages/Following/styles.ts
+++ src/Pages/Following/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+import colors from '../../styles/colors';
+import { Platform } from 'react-native';
+
+const statusbarHeight =
+	Platform.OS === 'android' ? Constants.statusBarHeight : 0;
+
+export const Wrapper = styled.SafeAreaView`
+	background-color: ${colors.primary};
+	flex: 1;
+	padding-top: ${statusbarHeight}px;
+`;
 
 export const Container = styled.View`
-
+	padding-left: 24px;
 `;
+
+export const Main = styled.View``;
